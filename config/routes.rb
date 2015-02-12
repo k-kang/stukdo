df38373562c1a55534e59aaae478bbed0ae37846
Kevin Kang <kang.kev@gmail.com>
2015-02-11 19:37:41 -0500

    added login in and logout conditions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
  
 
-  devise_for :users
+  
+  devise_for :users 
+   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -9,6 +11,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => "pages#about"
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
